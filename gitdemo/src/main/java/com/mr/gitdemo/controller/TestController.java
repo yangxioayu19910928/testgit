f35d54535d0aeecf4015d2532205a8726d64b812
yangwen-gitHub <345261241@qq.com>
2019-10-25 09:52:41 +0800

    修改测试

--- gitdemo/src/main/java/com/mr/gitdemo/controller/TestController.java
+++ gitdemo/src/main/java/com/mr/gitdemo/controller/TestController.java
@@ -3,12 +3,19 @@ package com.mr.gitdemo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.ws.rs.GET;
+
 @RestController
 public class TestController {
 
     @GetMapping("/testReq")
     public String testReq(){
         return "successful";
+    }
+
+    @GetMapping("/testUpdateReq")
+    public String testUpdateReq(){
+        return "updateSuccessful";
     }
 
 }
