b674074e9ccb21db72a64485da32ce0e2a59a999
yangwen-gitHub <345261241@qq.com>
2019-10-25 10:31:23 +0800

    修改测试

---
+++ gitdemo/src/main/java/com/mr/gitdemo/controller/Test1Controller.java
@@ -0,0 +1,10 @@
+package com.mr.gitdemo.controller;
+
+/**
+ * Created by yangwen on 2019/10/25.
+ */
+public class Test1Controller {
+    public static void main(String[] args) {
+        System.out.println(321);
+    }
+}
